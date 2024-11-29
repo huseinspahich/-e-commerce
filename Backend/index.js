2c2133f4dea9d0244f90fd3c1aac4c26724107c2
Harun Huseinspahic <harunhuseinspahic2001@gmail.com>
2024-11-30 00:19:41 +0100

    Connected front and back

--- Backend/index.js
+++ Backend/index.js
@@ -8,12 +8,10 @@ env.config();
 const app = express();
 const port = 3000;
 
-// Služi statičke slike iz foldera public/images
 app.use("/images", express.static("public/images"));
 app.use(cors());
 app.use(express.json());
 
-// Konekcija s bazom podataka
 const db = new pg.Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
@@ -30,7 +28,6 @@ db.connect((err) => {
   }
 });
 
-// Funkcija za unos proizvoda u bazu
 const insertProducts = async () => {
     try {
       const result = await db.query("SELECT COUNT(*) FROM products");
@@ -57,9 +54,49 @@ const insertProducts = async () => {
       console.error("Error inserting products:", error.message);
     }
   };
-  
-// Pokreni unos proizvoda nakon konekcije
 db.once("connect", insertProducts);
+
+app.get("/", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM products");
+      
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error fetching products:", error.message);
+      res.status(500).json({ error: "Failed to fetch products" });
+    }
+  });
+
+app.get("/men", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM products WHERE category = $1", ['men']);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error fetching products:", error.message);
+      res.status(500).json({ error: "Failed to fetch products" });
+    }
+  });
+
+app.get("/women", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM products WHERE category = $1", ['women']);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error fetching products:", error.message);
+      res.status(500).json({ error: "Failed to fetch products" });
+    }
+  });
+
+app.get("/kids", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM products WHERE category = $1", ['kids']);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error fetching products:", error.message);
+      res.status(500).json({ error: "Failed to fetch products" });
+    }
+  });
+
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
